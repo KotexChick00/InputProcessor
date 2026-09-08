78d5347b1e7c8422db7aa1b60a6c7679e58958e7
CatCode204 <truongtaman2004@gmail.com>
2026-09-08 07:31:37 +0700

    [Feat]: Link Opengl 3.3 to Project

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
+#ifdef _USE_GLAD_OPENGL
+#include <glad/glad.h>
+#endif
 #include <GLFW/glfw3.h>
 
 int main() {
 	if (!glfwInit()) {
 		std::cerr << "Failed to initialize GLFW" << std::endl;
+		glfwTerminate();
 		return -1;
 	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
 	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello, World!", NULL, NULL);
 	if (!window) {
@@ -15,6 +26,12 @@ int main() {
 	}
 
 	glfwMakeContextCurrent(window);
+	
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
 
 	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
@@ -22,6 +39,7 @@ int main() {
 		glfwPollEvents();
 	}
 
+	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
 }
